73c00bea96946f74dce53f169b24539ebb2fa0ed
yossibank <yossibank@yahoo.co.jp>
2021-12-26 11:20:36 +0900

    #24 fix resource management

--- SwiftArchitectureTemplate/Resource/Colors.swift
+++ SwiftArchitectureTemplate/Resource/Colors.swift
@@ -1,5 +1,21 @@
 import UIKit
 
 struct ColorResources {
-    static var sampleColor: UIColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    static let sampleColor: UIColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+}
+
+extension UIColor {
+
+    static var random: UIColor {
+        let red = CGFloat.random(in: 0...255) / 255.0
+        let blue = CGFloat.random(in: 0...255) / 255.0
+        let green = CGFloat.random(in: 0...255) / 255.0
+
+        return UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1.0
+        )
+    }
 }
