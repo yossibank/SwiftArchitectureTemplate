3c7cddf7fd4a2d1cf55da8e30129dbdb147ac4bc
yossibank <yossibank@yahoo.co.jp>
2021-12-29 15:05:07 +0900

    #30 modify spm setting

--- Packages/Utility/Package.swift
+++ Packages/Utility/Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "Utility",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v13)
     ],
     products: [
         .library(
